27e32298a2d0862139b04a319875519b4399a732
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-11-18 10:48:09 +0530

    Input box create for ticket choose

--- frontend/src/components/TicketBook.jsx
+++ frontend/src/components/TicketBook.jsx
@@ -1,127 +1,73 @@
-import { v4 as uuidv4 } from "uuid";
-import { MdOutlineFlightTakeoff } from "react-icons/md";
 import { useState } from "react";
+import TicketInput from "./TicketInput";
 function TicketBook({ locations: [{ location }] }) {
-  const [showOptions1, setShowOptions1] = useState(false);
-  const [showOptions2, setShowOptions2] = useState(false);
+  // const [showOptions1, setShowOptions1] = useState(false);
+  // const [showOptions2, setShowOptions2] = useState(false);
 
   const [formData, setFormData] = useState({
-    fromCity: location[0].city,
-    fromCityCode: location[0].cityCode,
-    fromAirport: location[0].airport,
-    toCity: location[1].city,
-    toCityCode: location[1].cityCode,
-    toAirport: location[1].airport,
+    from: {
+      city: location[0].city,
+      cityCode: location[0].cityCode,
+      airport: location[0].airport,
+      country: location[0].country,
+    },
+    to: {
+      city: location[1].city,
+      cityCode: location[1].cityCode,
+      airport: location[1].airport,
+      country: location[1].country,
+    },
   });
+
+  const { from, to } = formData;
 
   const onChangeFrom = (city, cityCode, airport, country) => {
     setFormData((prev) => ({
       ...prev,
-      fromCity: city,
-      fromCityCode: cityCode,
-      fromAirport: airport,
+      from: {
+        city,
+        cityCode,
+        airport,
+        country,
+      },
     }));
   };
   const onChangeTo = (city, cityCode, airport, country) => {
     setFormData((prev) => ({
       ...prev,
-      toCity: city,
-      toCityCode: cityCode,
-      toAirport: airport,
+      to: {
+        city,
+        cityCode,
+        airport,
+        country,
+      },
     }));
   };
 
-  const { fromCity, fromAirport, fromCityCode, toCity, toAirport, toCityCode } =
-    formData;
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="">
       <hr />
-      <form>
-        <div
-          className="flex flex-col my-6 border-[1px] border-[rgba(0,0,0,0.1)] p-2 rounded-md relative"
-          onClick={() => setShowOptions1((prev) => !prev)}
-        >
-          <label>From</label>
-          <input
-            className="capitalize text-xl font-bold bg-white"
-            value={fromCity}
-            disabled
-          />
-          <input
-            className="capitalize bg-white"
-            value={`${fromCityCode}, ${fromAirport}`.slice(0, 41) + "..."}
-            disabled
-          />
-          {showOptions1 && (
-            <div className="absolute top-24 z-10 border-2 bg-white max-h-56 overflow-y-auto">
-              {location.map(({ city, cityCode, airport, country }) => {
-                return (
-                  <div
-                    key={uuidv4()}
-                    className="flex items-center my-2"
-                    onClick={() =>
-                      onChangeFrom(city, cityCode, airport, country)
-                    }
-                  >
-                    <MdOutlineFlightTakeoff />
-                    <div className="flex flex-col">
-                      <h3 className="">
-                        <span>{city}</span>
-                        <span>{country}</span>
-                      </h3>
-                      <p>{airport}</p>
-                    </div>
-                    <h2 className="uppercase text-lg font-bold text-[rgba(0,0,0,0.6)]">
-                      {cityCode}
-                    </h2>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
-        <div
-          className="flex flex-col my-6 border-[1px] border-[rgba(0,0,0,0.1)] p-2 rounded-md relative"
-          onClick={() => setShowOptions2((prev) => !prev)}
-        >
-          <label>To</label>
-          <input
-            className="capitalize text-xl font-bold bg-white"
-            value={toCity}
-            disabled
-          />
-          <input
-            className="capitalize bg-white"
-            value={`${toCityCode}, ${toAirport}`.slice(0, 41) + "..."}
-            disabled
-          />
-          {showOptions2 && (
-            <div className="absolute top-24 z-10 border-2 bg-white max-h-56 overflow-y-auto">
-              {location.map(({ city, cityCode, airport, country }) => {
-                return (
-                  <div
-                    key={uuidv4()}
-                    className="flex items-center my-2"
-                    onClick={() => onChangeTo(city, cityCode, airport, country)}
-                  >
-                    <MdOutlineFlightTakeoff />
-                    <div className="flex flex-col">
-                      <h3 className="">
-                        <span>{city}</span>
-                        <span>{country}</span>
-                      </h3>
-                      <p>{airport}</p>
-                    </div>
-                    <h2 className="uppercase text-lg font-bold text-[rgba(0,0,0,0.6)]">
-                      {cityCode}
-                    </h2>
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
-        <button className="w-full btn btn-primary">Search</button>
+      <form onSubmit={onSubmit}>
+        <TicketInput
+          label="From"
+          location={from}
+          onChangeFn={onChangeFrom}
+          allLocation={location}
+        />
+        <TicketInput
+          label="To"
+          location={to}
+          onChangeFn={onChangeTo}
+          allLocation={location}
+        />
+        <button type="submit" className="w-full btn btn-primary">
+          Search
+        </button>
       </form>
     </div>
   );
---
+++ frontend/src/components/TicketInput.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { MdOutlineFlightTakeoff } from "react-icons/md";
+
+function TicketInput({ location, onChangeFn, allLocation, label }) {
+  const [showOptions, setShowOptions] = useState(false);
+  const { city, cityCode, airport } = location;
+  return (
+    <div
+      className="flex flex-col my-6 border-[1px] border-[rgba(0,0,0,0.1)] p-2 rounded-md relative"
+      onClick={() => setShowOptions((prev) => !prev)}
+    >
+      <label>{label}</label>
+      <input
+        className="capitalize text-xl font-bold bg-white"
+        value={city}
+        disabled
+      />
+      <div className="flex">
+        <input
+          className="uppercase w-[13%] bg-white"
+          value={cityCode}
+          disabled
+        />
+        <input
+          className="capitalize w-[87%] bg-white"
+          value={`, ${airport}`.substring(0, 39) + "..."}
+          disabled
+        />
+      </div>
+      {showOptions && (
+        <div className="absolute px-3 w-[100%] top-24 right-0 z-10 border-[1px] rounded-sm bg-white max-h-56 overflow-y-auto">
+          {allLocation.map(({ city, cityCode, airport, country }) => {
+            return (
+              <div
+                key={uuidv4()}
+                className="flex justify-between items-center my-2"
+                onClick={() => onChangeFn(city, cityCode, airport, country)}
+              >
+                <div className="flex items-center">
+                  <MdOutlineFlightTakeoff />
+                  <div className="flex flex-col ml-3">
+                    <h3 className="capitalize">
+                      <span className="font-bold text-[rgba(0,0,0,0.7)]">
+                        {city}
+                      </span>
+                      <span className="mx-1 uppercase">{country}</span>
+                    </h3>
+                    <p className="text-sm capitalize">{airport}</p>
+                  </div>
+                </div>
+                <h2 className="uppercase text-lg font-bold text-[rgba(0,0,0,0.6)]">
+                  {cityCode}
+                </h2>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default TicketInput;
