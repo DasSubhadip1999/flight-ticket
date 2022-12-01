aac52080dd022a4ad40a78874a289c3cd9469dd6
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-12-01 10:40:04 +0530

    heroku config add

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const color = require("colors");
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 
@@ -12,16 +13,25 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200);
-  res.json({ message: "Welcome to flight ticket API" });
-});
-
 app.use("/api/users", require("./routes/userRouter"));
 app.use("/api/flights", require("./routes/flightRouter"));
 app.use("/api/flights/airlines", require("./routes/airlineRouter"));
 app.use("/api/tickets", require("./routes/ticketRouter"));
 app.use(errorHandler);
+
+//server the front end
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html");
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200);
+    res.json({ message: "Welcome to flight ticket API" });
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`.yellow.underline);
